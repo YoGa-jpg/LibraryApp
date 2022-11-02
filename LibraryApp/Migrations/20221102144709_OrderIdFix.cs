03325a9e39cfb0147af4a269a069206b48ba1d0b
YoGa-jpg <antondavidovich45@gmail.com>
2022-11-02 19:05:51 +0300

    Migrations

---
+++ LibraryApp/Migrations/20221102144709_OrderIdFix.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LibraryApp.Migrations
+{
+    public partial class OrderIdFix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "OrderId",
+                table: "Books",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "OrderId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "OrderId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "OrderId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "OrderId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "OrderId",
+                value: null);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "OrderId",
+                table: "Books",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "OrderId",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "OrderId",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "OrderId",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "OrderId",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "OrderId",
+                value: 0);
+        }
+    }
+}
---
+++ LibraryApp/Migrations/20221102150126_StartValuesFix.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LibraryApp.Migrations
+{
+    public partial class StartValuesFix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "OrderId",
+                value: 2);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "OrderId",
+                value: null);
+        }
+    }
+}
